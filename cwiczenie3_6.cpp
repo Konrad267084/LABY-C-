cbc7958b67fe034c64f13216f49dcae3cbdecb6a
Konrad <olejnik.konrad@gmail.com>
2023-03-16 02:45:15 +0100

    Zadanie 3_6 wersja 1

---
+++ cwiczenie3_6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+	std::cout << "Enter three numbers" << "\n";
+	int first_number, second_number, third_number;
+	std::cin >> first_number >> second_number >> third_number;
+	if (first_number > second_number) {
+		std::swap(first_number, second_number);
+	} 
+	if (second_number > third_number) {
+		std::swap(second_number, third_number);
+	}
+	if (first_number > second_number) {
+		std::swap(first_number, second_number);
+	}
+	std::cout << "Sequence is " << first_number << " " << second_number << " " << third_number;
+
+
+
+
+
+
+
+}
