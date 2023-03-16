f665f6f137730c9accb58cf3a5d2ea61c5611499
Konrad <olejnik.konrad@gmail.com>
2023-03-16 03:07:37 +0100

    Wersja 2

--- cwiczenie3_6.cpp
+++ cwiczenie3_6.cpp
@@ -6,7 +6,7 @@ int main() {
 	std::cin >> first_number >> second_number >> third_number;
 	if (first_number > second_number) {
 		std::swap(first_number, second_number);
-	} 
+	}
 	if (second_number > third_number) {
 		std::swap(second_number, third_number);
 	}
@@ -14,11 +14,4 @@ int main() {
 		std::swap(first_number, second_number);
 	}
 	std::cout << "Sequence is " << first_number << " " << second_number << " " << third_number;
-
-
-
-
-
-
-
 }
---
+++ cwieczenie3_8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main() {
+	std::cout << "Enter the number" << "\n";
+	int number;
+	std::cin >> number;
+	if (number % 2 == 0) {
+		std::cout << "The value " << number << " is an even number";
+	} else
+		std::cout << "The value " << number << " is an odd number";
+
+
+
+
+}
